fdde6d0ae0d28e7539269c6db96f4a20669b5c6e
Justin Baumgartner <justbaum30@gmail.com>
2014-06-26 11:51:00 -0500

    Add LocationService with minimal PlayService initialization code.

--- src/android/BackgroundLocationPlugin.java
+++ src/android/BackgroundLocationPlugin.java
@@ -1,33 +1,62 @@
-package com.justinbaumgartner.cordova.geolocationservice;
+package com.justinbaumgartner.cordova.backgroundlocation;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaWebView;
-import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
-import android.util.Log;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.widget.Toast;
 
-public class GeoLocationService extends CordovaPlugin {
+public class BackgroundLocationPlugin extends CordovaPlugin {
+	
+	public LocationService service;
+	private ServiceConnection connection = new ServiceConnection() {
+
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			LocationService.LocalBinder binder = (LocationService.LocalBinder)service;
+			BackgroundLocationPlugin.this.service = binder.getService();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+		}
+	};
+	
+	private final static String TAG = "BackgroundLocationPlugin";
+	
 
 	@Override
 	public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 		super.initialize(cordova, webView);
+		
+		Intent intent = new Intent(cordova.getActivity(), LocationService.class);
+		cordova.getActivity().bindService(intent, connection, Context.BIND_AUTO_CREATE);
 	}
 	
 	@Override
-	public void execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("test")) {
-			Log.d("test called");
+			Toast.makeText(webView.getContext(), "test called", Toast.LENGTH_SHORT).show();
 			return true;
 		}
 		else if (action.equals("deviceready")) {
-			Log.d("deviceready called")
+			Toast.makeText(webView.getContext(), "device is ready", Toast.LENGTH_SHORT).show();
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public void onDestroy() {
+		cordova.getActivity().unbindService(connection);
+		super.onDestroy();
 	}
 }
---
+++ src/android/LocationService.java
@@ -0,0 +1,108 @@
+package com.justinbaumgartner.cordova.backgroundlocation;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
+public class LocationService extends Service implements GooglePlayServicesClient.ConnectionCallbacks, GooglePlayServicesClient.OnConnectionFailedListener {
+	
+	private final IBinder binder = new LocalBinder();
+	private static final String TAG = "LocationService";
+	
+	
+	@Override
+	public void onCreate(){
+		super.onCreate();
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		super.onStartCommand(intent, flags, startId);
+		
+		return START_STICKY;
+	}
+	
+	/**
+     * Check if user has connection to Google Play services
+     */
+	public boolean isServicesConnected() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            Log.d(TAG, "Google Play services is available.");
+            return true;
+        }
+        // Google Play services was not available for some reason
+        else {
+            Log.e(TAG, "Google Play services is not available with result code - " + resultCode);
+            return false;
+        }
+    }
+	
+	/**
+     * Called by Location Services when the request to connect the
+     * client finishes successfully. At this point, you can
+     * request the current location or start periodic updates
+     */
+	@Override
+	public void onConnected(Bundle data) {
+		Log.d(TAG, "Location services connected.");
+	}
+
+	/**
+     * Called by Location Services if the connection to the
+     * location client drops because of an error.
+     */
+	@Override
+	public void onDisconnected() {
+		Log.d(TAG, "Location services disconnected.");
+	}
+	
+	/**
+     * Called by Location Services if the attempt to
+     * Location Services fails.
+     */
+	@Override
+	public void onConnectionFailed(ConnectionResult connectionResult) {
+//		// Check if Google Play services can resolve the error.
+//        if (connectionResult.hasResolution()) {
+//            try {
+//            	
+//                // Start an Activity that tries to resolve the error
+//                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+//            }
+//            // Thrown if Google Play services canceled the original PendingIntent
+//            catch (IntentSender.SendIntentException e) {
+//                // Log the error
+//                e.printStackTrace();
+//            }
+//        }
+//        // No resolution available
+//        else {
+//            Log.e(TAG, "Connection to location services failed with no resolution.");
+//        }
+		Log.e(TAG, "Connection to location services failed.");
+	}
+	
+	public class LocalBinder extends Binder {
+		LocationService getService() {
+			return LocationService.this;
+		}
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent) {
+		return binder;
+	}
+
+	
+}
