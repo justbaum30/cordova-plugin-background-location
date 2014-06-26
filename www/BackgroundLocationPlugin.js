af4c464ba76ffeb8260017a1e4862fa6c0ddb247
Justin Baumgartner <justbaum30@gmail.com>
2014-06-26 14:05:39 -0500

    Update js file with new name and bug fixes.

--- www/BackgroundLocationPlugin.js
+++ www/BackgroundLocationPlugin.js
@@ -1,4 +1,8 @@
-GeoLocationService.prototype = {
+var BackgroundLocationPlugin = function() {
+
+};
+
+BackgroundLocationPlugin.prototype = {
 
 	createCallbackFn: function (callbackFn, scope) {
 		if (typeof callbackFn !== 'function')
@@ -12,16 +16,16 @@ GeoLocationService.prototype = {
 	test: function (callback, scope) {
 		console.log('test');
 		var callbackFn = this.createCallbackFn(callback, scope);
-		cordova.exec(callbackFn, null, 'GeoLocationService', 'test', []);
+		cordova.exec(callbackFn, null, 'BackgroundLocationPlugin', 'test', []);
 	}
 };
 
-var plugin = new GeoLocationService();
+var plugin = new BackgroundLocationPlugin();
 var channel = require('cordova/channel');
 
 channel.deviceready.subscribe(function () {
 	console.log('channel.deviceready');
-	cordova.exec(null, null, 'GeoLocationService', 'deviceready', []);
+	cordova.exec(null, null, 'BackgroundLocationPlugin', 'deviceready', []);
 });
 
 module.exports = plugin;
